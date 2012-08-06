a6efa014b4267c6da97e2f41b147c66afd9a54bb
Peter Cho <pcho@phase2technology.com>
2012-08-06 17:08:43 -0400

    Made box width and height configurable.  Added content configuration textarea.  Cleaned up style code.

--- theme/content_overlay.tpl.php
+++ theme/content_overlay.tpl.php
@@ -1,9 +1,9 @@
 <div id="content-overlay">
-  <div id="dialog" class="window">
+  <div id="dialog" class="window" style="width: <?php print $box_width; ?>; height: <?php print $box_height; ?>;">
     <a href="#" class="close"><?php print t('Close'); ?></a>
     <div class="content">
-    	asdfsafd
-	  </div>
+    	Advertisements
+	</div>
   </div>
 
   <div id="mask"></div>
